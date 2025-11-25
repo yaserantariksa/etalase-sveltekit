869bbd0f5e1b036cdcba4c5b86263525b8efd931
yaserantariksa <yaserantariksa@gmail.com>
2025-11-25 21:38:26 +0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let message = $state('Loading...');
+  
+  onMount(async () => {
+    await fetch('/api/hello-world').then(async (result) => {
+      setTimeout(async () => {
+        message = await result.text();
+      }, 2000);
+    })
+  });
+</script>
+
+<div>
+	<h1>{message}</h1>
+</div>
